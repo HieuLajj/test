b78de7c8a966c82e51a79bae7956c03a5f0856ff
HieuLajj <laihieu098113@gmail.com>
2023-10-08 20:03:15 +0700

    8_10

--- Assets/Scripts/SaveData/Item.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "Test1", menuName = "ItemsData")]
-public class Item :  ScriptableObject
-{
-    public int itemId;
-    public string itemName;
-}
